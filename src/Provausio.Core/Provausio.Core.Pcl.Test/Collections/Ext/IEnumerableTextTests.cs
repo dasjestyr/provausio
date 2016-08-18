1089f8a49c17dbf494b842b6ea082571c7658ce7
Jeremy Stafford <draepetan@live.com>
2016-08-17 19:14:59 -0700

    added enumerable chunk extention with test coverage

---
+++ src/Provausio.Core/Provausio.Core.Pcl.Test/Collections/Ext/IEnumerableTextTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Provausio.Core.Pcl.Collections.Ext;
+using Xunit;
+
+namespace Provausio.Core.Pcl.Test.Collections.Ext
+{
+    public class EnumerableChunkTests
+    {
+        [Fact]
+        public void Chunk_NullList_Throws()
+        {
+            // arrange
+            List<int> list = null;
+
+            // act
+
+            // assert
+            Assert.Throws<ArgumentNullException>(() => list.Chunk(100).ToList());
+
+        }
+
+        [Fact]
+        public void Chunk_EmptyList_ReturnsSameList()
+        {
+            // arrange
+            var list = new List<int>();
+
+            // act
+            var result = list.Chunk(100);
+
+            // assert
+            Assert.Equal(0, result.Count());
+        }
+
+        [Theory]
+        [InlineData(10, 5)]
+        [InlineData(11, 5)]
+        [InlineData(103, 5)]
+        public void Chunk_ValidList_ReturnsExpectedChunks(int listSize, int chunkSize)
+        {
+            // arrange
+            var collection = GenFu.GenFu.ListOf<int>(listSize);
+            var expectedChunkCount = (listSize/chunkSize) + (listSize%chunkSize == 0 ? 0 : 1);
+
+            // act
+            var chunk = collection.Chunk(chunkSize);
+
+            // assert
+            Assert.Equal(expectedChunkCount, chunk.Count());
+        }
+
+        [Fact]
+        public void Chunk_CanIterate()
+        {
+            // arrange
+            var collection = GenFu.GenFu.ListOf<int>(100);
+            var expectedSum = collection.Sum();
+
+            // act
+            var accumulator = 0;
+            foreach (var chunk in collection.Chunk(5))
+            {
+                accumulator += chunk.Sum();
+            }
+
+            // assert
+            Assert.Equal(expectedSum, accumulator);
+        }
+    }
+}
---
+++ src/Provausio.Core/Provausio.Core.Pcl/Collections/Ext/IEnumerableExt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Provausio.Core.Pcl.Collections.Ext
+{
+    public static class EnumerableChunk
+    {
+        /// <summary>
+        /// Chunks the specified size. This method will iterate the source list.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source">The source.</param>
+        /// <param name="size">The size.</param>
+        /// <returns></returns>
+        public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> source, int size)
+        {
+            if(source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            var sourceArr = source as T[] ?? source.ToArray();
+
+            while (sourceArr.Any())
+            {
+                yield return sourceArr.Take(size);
+                sourceArr = sourceArr.Skip(size).ToArray();
+            }
+        }
+    }
+}
